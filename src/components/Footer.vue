b851cc9c8489159c2206b2cc07735bf933f0aa60
irene <user@MacBook-Pro-de-User.local>
2025-01-22 23:34:48 +0100

    components basic

---
+++ src/components/Footer.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+p FOOTER
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-  </main>
+<template lang="pug">
+Main
 </template>
+<script setup lang="ts">
+import Main from '../components/Main.vue'
+</script>
