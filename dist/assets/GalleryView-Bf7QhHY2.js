942681d388a5fcaa1c9a35259f43ec975d985534
airin181 <iregmerino@gmail.com>
2025-02-02 15:35:45 +0100

    adding compiled files

---
+++ dist/assets/GalleryView-Bf7QhHY2.js
@@ -0,0 +1,1 @@
+import{d as s,u as t,c as a,a as o,o as n}from"./index-l0frZJvI.js";const r={class:"gallery view"},i=s({__name:"GalleryView",setup(l){const{t:c}=t();return(p,e)=>(n(),a("div",r,e[0]||(e[0]=[o("p",null,"GALLERY VIEW",-1)])))}});export{i as default};
