b218a5c26c58c5ad5a777d64626009ea1a5039ab
airin181 <iregmerino@gmail.com>
2025-02-22 20:44:00 +0100

    subtitle label + fix textarea + button hover update

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -6,7 +6,7 @@
   .contact-body
     section
       h3.title {{ $t("contact.title") }}
-      p.p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat:
+      p.p {{ $t("contact.subtitle") }}
     section
       ContactForm
     section
