e299241b6ee0ae3c3d7c4128b3167ded835ed7a4
airin181 <iregmerino@gmail.com>
2025-03-03 19:31:06 +0100

    go to top button

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 NavHeader
 RouterView
 MainFooter
+GoToTopButton
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavHeader from './components/header/NavHeader.vue'
 import MainFooter from './components/MainFooter.vue'
+import GoToTopButton from './components/common/GoToTopButton.vue'
 </script>
---
+++ src/components/common/GoToTopButton.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .b-button-go-top(@click='scrollToTop', v-if="showButton")
+    i.mdi.mdi-arrow-up-thin
+  </template>
+
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount, ref } from 'vue'
+
+const showButton = ref(false)
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+
+function scrollToTop() {
+  window.scrollTo({ left: 0, top: 0, behavior: 'smooth' })
+}
+
+function handleScroll() {
+  if (window.scrollY > 250) {
+    showButton.value = true
+  } else {
+    showButton.value = false
+  }
+}
+</script>
