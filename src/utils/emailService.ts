a1ac58c369576121d2235f62030fb28d758fe78b
airin181 <iregmerino@gmail.com>
2025-03-21 00:23:39 +0100

    api url update

--- src/utils/emailService.ts
+++ src/utils/emailService.ts
@@ -1,6 +1,7 @@
 import type { ContactForm } from '@/types/_types'
 import axios from 'axios'
-const apiUrl = `${import.meta.env.VITE_APP_API_URL}/send-email`
+const apiUrl =
+  import.meta.env.VITE_APP_API_URL + '/send-email' || import.meta.env.VITE_HOST + '/send-email'
 
 export const sendEmail = async (emailData: ContactForm) => {
   try {
