a90d9a0a9c82631c6c45c0913bf7ef60f870c441
airin181 <iregmerino@gmail.com>
2025-03-31 09:12:36 +0200

    email service update

--- src/utils/emailService.ts
+++ src/utils/emailService.ts
@@ -1,15 +1,25 @@
 import type { ContactForm } from '@/types/_types'
 import axios from 'axios'
-const apiUrl =
-  import.meta.env.VITE_APP_API_URL + '/api' + '/send-email' ||
-  import.meta.env.VITE_HOST + '/send-email'
+const apiUrl = import.meta.env.VITE_APP_API_URL
+  ? `${import.meta.env.VITE_APP_API_URL}/api/send-email`
+  : import.meta.env.VITE_HOST
+    ? `${import.meta.env.VITE_HOST}/send-email`
+    : ''
 
 export async function sendEmail(emailData: ContactForm) {
-  try {
+  if (!apiUrl) {
+    console.error('❌ Error: API_URL is not defined.')
+    return
+  }
 
-    const response = await axios.post(apiUrl, emailData, { withCredentials: true })
+  try {
+    const response = await axios.post(apiUrl, emailData, {
+      withCredentials: true,
+      headers: { 'Content-Type': 'application/json' },
+    })
     return response
   } catch (error) {
+    console.error('❌ Request error:', error)
     throw error
   }
 }
