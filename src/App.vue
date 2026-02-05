239f82846f5c75abf0da3a2f0051084b916d2be7
airin181 <iregmerino@gmail.com>
2026-02-05 19:35:17 +0100

    test code deleted

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,4 @@ import { RouterView } from 'vue-router'
 import NavHeader from './components/header/NavHeader.vue'
 import MainFooter from './components/MainFooter.vue'
 import GoToTopButton from './components/common/GoToTopButton.vue'
-import axios from 'axios'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  axios('/api/test')
-    .then((response) => {
-      console.log('API response:', response.data)
-    })
-    .catch((error) => {
-      console.error('Error fetching API:', error)
-    })
-})
 </script>
